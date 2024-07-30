3140306b5900747a85336f87adc5d7234bef4b5f
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2024-07-30 18:55:50 +0200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
-'''
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    '''default route'''
-    return render_template("0-index.html",)
+@app.route("/")
+def home():
+    """Home page route"""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
