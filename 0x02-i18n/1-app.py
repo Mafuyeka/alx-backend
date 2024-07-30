1fae5b00ab5fd01ccc57ee2b0d7efb13bd9253d8
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2024-07-30 18:55:23 +0200

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
-'''
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config:
-    '''Config class'''
+class Config(object):
+    """Flask configs"""
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -16,15 +14,13 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
-
 babel = Babel(app)
 
 
-@app.route('/')
-def index():
-    '''default route'''
-    return render_template("1-index.html",)
+@app.route("/")
+def home():
+    """Home page route"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
